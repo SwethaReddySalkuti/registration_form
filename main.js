749692a47aa6d3ac285005f1a4e773720679ee45
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-02-28 15:32:01 +0530

    Object js

--- main.js
+++ main.js
@@ -11,13 +11,19 @@ function onSubmit(e)
 {
     e.preventDefault();
 
+
     if(nameInput.value === '' ||emailInput.value=== '' )
     {
         alert('Please Enter details');
     }
     else
     {
-        localStorage.setItem(nameInput.value,emailInput.value);
+        const name = e.target.nameInput.value;
+        const email = e.target.emailInput.value;
+        const obj = {
+            name : name,
+            email : email
+        }
     }
 
 }
